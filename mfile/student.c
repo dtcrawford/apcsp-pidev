8acbd74915acaf144a973814bf5fd388f4a2eb19
Dylan Crawford <crawfordd0951@my.sduhsd.net>
2020-01-10 14:07:03 -0800

    final commit

---
+++ mfile/student.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "student.h"
+void printStudent(struct Student* student){
+    printf("%s %s:\n", student->first, student->last);
+    printf("age: %d\n",student->age);
+    printf("student ID: %d\n\n", student->id);
+}
